c0d7969bac54cb379c247b520d6b5206c181e013
Wu Zhe <wu@madk.org>
2011-06-29 15:46:40 +0800

    Delete useless and private files

--- bin/stopvpn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo /usr/sbin/ipsec auto --down L2TP-PSK
-/bin/echo "d vpn-connection" | sudo tee -a /var/run/xl2tpd/l2tp-control
-sudo /etc/rc.d/xl2tpd stop
-sudo /etc/rc.d/openswan stop
-
-sudo route del 65.49.73.162 gw 192.168.1.1 wlan0
-sudo route add default gw 192.168.1.1

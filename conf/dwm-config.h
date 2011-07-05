27d3fbb08a68dc8ca58020e76c1e67807a8c16aa
Wu Zhe <wu@madk.org>
2011-07-05 22:13:50 +0800

    Fix a dwm float config for mplayer

--- conf/dwm-config.h
+++ conf/dwm-config.h
@@ -20,7 +20,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 3,       True,        -1 },
-	{ "Mplayer",  NULL,       NULL,       1 << 7,       True,        -1 },
+	{ "MPlayer",  NULL,       NULL,       1 << 7,       True,        -1 },
 };
 
 /* layout(s) */
